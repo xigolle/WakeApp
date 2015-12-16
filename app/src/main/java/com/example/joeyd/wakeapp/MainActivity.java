4c99ab36c572a2e12712df89179337287444597a
xigolle <joey.driessen@live.nl>
2015-12-16 01:56:45 +0100

    Example code to call send function

--- app/src/main/java/com/example/joeyd/wakeapp/MainActivity.java
+++ app/src/main/java/com/example/joeyd/wakeapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //how to call the send function
+//        TextManager.Send("0487174475","testing",this);
     }
 
     @Override
